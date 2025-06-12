05d6f441966e526f14dcc37b4294299435dc3505
JCAtlas08 <campjosh21@gmail.com>
2025-06-12 01:46:46 +0000

    changed a lot of things

--- workspace/src/gamelogic/level/Level.java
+++ workspace/src/gamelogic/level/Level.java
@@ -16,6 +16,7 @@ import gamelogic.tiledMap.Map;
 import gamelogic.tiles.Flag;
 import gamelogic.tiles.Flower;
 import gamelogic.tiles.Gas;
+import gamelogic.tiles.Lava;
 import gamelogic.tiles.SolidTile;
 import gamelogic.tiles.Spikes;
 import gamelogic.tiles.Tile;
@@ -35,6 +36,9 @@ public class Level {
 
 	private ArrayList<Enemy> enemiesList = new ArrayList<>();
 	private ArrayList<Flower> flowers = new ArrayList<>();
+	private ArrayList<Water> waterList = new ArrayList<>();
+	private ArrayList<Gas> gasList = new ArrayList<>();
+	private ArrayList<Lava> lavaList = new ArrayList<>();
 
 	private List<PlayerDieListener> dieListeners = new ArrayList<>();
 	private List<PlayerWinListener> winListeners = new ArrayList<>();
@@ -118,17 +122,20 @@ public class Level {
 					tiles[x][y] = new Water(xPosition, yPosition, tileSize, tileset.getImage("Half_water"), this, 2);
 				else if (values[x][y] == 21)
 					tiles[x][y] = new Water(xPosition, yPosition, tileSize, tileset.getImage("Quarter_water"), this, 1);
-			}
+				else if (values[x][y] == 22)
+					tiles[x][y] = new Lava(xPosition, yPosition, tileSize, tileset.getImage("Lava"), this);
 
+			}
 		}
+
 		enemies = new Enemy[enemiesList.size()];
 		map = new Map(width, height, tileSize, tiles);
 		camera = new Camera(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT, 0, map.getFullWidth(), map.getFullHeight());
 		for (int i = 0; i < enemiesList.size(); i++) {
 			enemies[i] = new Enemy(enemiesList.get(i).getX(), enemiesList.get(i).getY(), this);
 		}
-		player = new Player(leveldata.getPlayerX() * map.getTileSize(), leveldata.getPlayerY() * map.getTileSize(),
-				this);
+
+		player = new Player(leveldata.getPlayerX() * map.getTileSize(), leveldata.getPlayerY() * map.getTileSize(), this);
 		camera.setFocusedObject(player);
 
 		active = true;
@@ -136,9 +143,20 @@ public class Level {
 		playerWin = false;
 	}
 
+	//Precondition: none
+	//Postcondition: Player returns to normal
+	public void resetPlayerEffects() {
+		player.jumpPower = 1350;
+		player.walkSpeed = 400;
+		Level.GRAVITY = 70;
+	}
+
 	public void onPlayerDeath() {
 		active = false;
 		playerDead = true;
+		waterList.clear();
+		gasList.clear();
+		resetPlayerEffects();
 		throwPlayerDieEvent();
 	}
 
@@ -184,6 +202,27 @@ public class Level {
 				}
 			}
 
+			//When in either gas or water, jump power is reduced
+			ArrayList<Tile> liquids = new ArrayList<>();
+			liquids.addAll(waterList);
+			liquids.addAll(gasList);
+			for (Tile t : liquids) {
+				if (!t.getHitbox().isIntersecting(player.getHitbox())) {
+					resetPlayerEffects();
+				}
+			}
+			//The higher the if statement, the higher priority
+			for (Tile t : liquids) {
+				if (t instanceof Water && t.getHitbox().isIntersecting(player.getHitbox())) {
+					player.jumpPower = 900;
+					player.walkSpeed = 200;
+
+				} else if (t instanceof Gas && t.getHitbox().isIntersecting(player.getHitbox())) {
+					player.jumpPower = 675;
+					Level.GRAVITY = 35;
+				}
+			}
+
 			// Update the map
 			map.update(tslf);
 
@@ -192,16 +231,25 @@ public class Level {
 		}
 	}
 	
+	//Precondition: none
+	//Postcondition: 1-20 new gas tiles are created
 	private void addGas(int col, int row, Map map, int numSquaresToFill, ArrayList<Gas> placedThisRound) {
+		//Records the new gas tile
 		Gas g = new Gas(col, row, tileSize, tileset.getImage("GasOne"), this, 0);
 		map.addTile(col, row, g);
 		placedThisRound.add(g);
-		numSquaresToFill -= 1;
+		gasList.add(g);
 
+		//Removes one because this is the starting gas tile
+		numSquaresToFill -= 1;
+		
+		//This loop allows us to look through every tile, plus all the new ones
 		for (int a = 0; a < placedThisRound.size(); a++) {
+			//Overwrites the original to use fewer variables and help navigate easier
 			col = placedThisRound.get(a).getCol();
 			row = placedThisRound.get(a).getRow();
 			
+			//A simple 2D Array to help navigate around all 8 spaces around the given tile in order
 			int[][] checkTiles = {
 				{col, row - 1}, // Top
 				{col + 1, row - 1}, // Top Right
@@ -212,28 +260,35 @@ public class Level {
 				{col + 1, row + 1}, // Bottom Right
 				{col - 1, row + 1} // Bottom Left
 			};
-
+			
+			//This loops through the 2D Array to check all 8 tiles around the given tile
 			for (int i = 0; i < checkTiles.length; i++) {
 				int c = checkTiles[i][0];
 				int r = checkTiles[i][1];
-
+				
+				//Checks to see if the given coordinates are within bounds and the tile at said coordinates is not solid nor a gas tile
 				if ((c > 0 && c < map.getTiles().length) && (r > 0 && r < map.getTiles()[0].length) && !map.getTiles()[c][r].isSolid() && !(map.getTiles()[c][r] instanceof Gas) && numSquaresToFill > 0) {
+					//Records the new gas tile
 					Gas newGas = new Gas(c, r, tileSize, tileset.getImage("GasOne"), this, 0);
-
 					map.addTile(c, r, newGas);
+					gasList.add(newGas);
 					placedThisRound.add(newGas);
+
 					numSquaresToFill -= 1;
 				}
 			}
 			
+			//Ensures to end the loop when there are no more squares left to fill
 			if (!(numSquaresToFill > 0)) {
 				break;
 			}
 		}
 	}
 
-
+	//Preconditions: none
+	//Postconditions: water spreads throughout the level as long as there are open spaces
 	private void water(int col, int row, Map map, int fullness) {
+		//This part is used so that when a new water tile is made, it takes the fullness and knows which costume from the costume map to use
 		String waterLevel = "";
 		if (fullness == 3) {
 			waterLevel = "Full_water";
@@ -245,36 +300,69 @@ public class Level {
 			waterLevel = "Falling_water";
 		}
 
+		//This records the new tile. Because the water method is recursive, we do not have to record water again.
 		Water w = new Water(col, row, tileSize, tileset.getImage(waterLevel), this, fullness);
 		map.addTile(col, row, w);
+		waterList.add(w);
 
-        //check if we can go down
+		//Check the tile below for an open space
 		if (row + 1 < map.getTiles()[0].length && !map.getTiles()[col][row + 1].isSolid()) {
-			water(col, row + 1, map, 0);
+			water(col, row + 1, map, 0); //Place water in the tile below with the falling costume
 
 		} else {
-			if (fullness == 0 && row - 1 > 0 && (map.getTiles()[col][row - 1] instanceof Water)) {
-				water(col, row, map, 3);
-			}
-			//if we can’t go down go left and right.
-			//right
-			if (col + 1 < map.getTiles().length && !(map.getTiles()[col + 1][row] instanceof Water) && !map.getTiles()[col + 1][row].isSolid()) {
-				if (fullness > 1) {
-					water(col + 1, row, map, fullness - 1);
+			if (row + 1 < map.getTiles()[0].length) {
+				//If this tile is currently falling, and the tile above is also a water tile, become a full water tile
+				if (fullness == 0 && row - 1 > 0 && (map.getTiles()[col][row - 1] instanceof Water)) { 
+					water(col, row, map, 3); //Place water 
 				}
-			}
-			//left
-			if (col - 1 >= 0 && !(map.getTiles()[col - 1][row] instanceof Water) && !map.getTiles()[col - 1][row].isSolid()) {
-				if (fullness > 1) {
-					water(col - 1, row, map, fullness - 1);
+
+				//If the tile below is solid, try to go left and right
+				//Right
+				if (col + 1 < map.getTiles().length - 1 && !(map.getTiles()[col + 1][row] instanceof Water) && !map.getTiles()[col + 1][row].isSolid()) {
+					if (fullness > 1) {
+						water(col + 1, row, map, fullness - 1);
+					} else {
+						water(col + 1, row, map, fullness);
+					}
+				}
+
+				//Left
+				if (col - 1 > 0 && !(map.getTiles()[col - 1][row] instanceof Water) && !map.getTiles()[col - 1][row].isSolid()) {
+					if (fullness > 1) {
+						water(col - 1, row, map, fullness - 1);
+					} else {
+						water(col - 1, row, map, fullness);
+					}
 				}
 			}
 		}
-
-        
 	}
 
+	private void lava(int col, int row, Map map) {
+		//This records the new tile. Because the water method is recursive, we do not have to record water again.
+		Lava l = new Lava(col, row, tileSize, tileset.getImage("lava"), this);
+		map.addTile(col, row, l);
+		lavaList.add(l);
 
+		//Check the tile below for an open space
+		if (row + 1 < map.getTiles()[0].length && !map.getTiles()[col][row + 1].isSolid()) {
+			lava(col, row + 1, map); //Place lava in the tile below with the falling costume
+
+		} else {
+			if (row + 1 < map.getTiles()[0].length) {
+				//If the tile below is solid, try to go left and right
+				//Right
+				if (col + 1 < map.getTiles().length - 1 && !(map.getTiles()[col + 1][row] instanceof Lava) && !map.getTiles()[col + 1][row].isSolid()) {
+					lava(col + 1, row, map);
+				}
+
+				//Left
+				if (col - 1 > 0 && !(map.getTiles()[col - 1][row] instanceof Lava) && !map.getTiles()[col - 1][row].isSolid()) {
+					lava(col - 1, row, map);
+				}
+			}
+		}
+	}
 
 	public void draw(Graphics g) {
 	   	g.translate((int) -camera.getX(), (int) -camera.getY());
